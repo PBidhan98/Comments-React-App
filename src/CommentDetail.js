81cc454759472d6cafe608cc870c1262e748b7de
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-13 13:28:36 +0530

    Passing and receiving props btw components

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import faker from'faker';
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
         <img alt="avatar" src={faker.image.avatar()}/>
       </a>
       <div className="content">
-        <a href="/" className="author">Sam</a>
+        <a href="/" className="author">{props.author}</a>
         <div className="metadata">
           <span className="date">Today at 6:00PM</span>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from'faker';
 import CommentDetail from './CommentDetail';
 //. means it's present in the same folder
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail author="Sam"/>
+      <CommentDetail author="Alex"/>
+      <CommentDetail author="Jane"/>
     </div>
   );
 };
