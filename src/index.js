cda6a4e6f8faac35b1186cd1adb97102646524d3
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-13 13:17:44 +0530

    Component Nesting

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from'faker';
+import CommentDetail from './CommentDetail';
+//. means it's present in the same folder
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()}/>
-        </a>
-        <div className="content">
-          <a href="/" className="author">Sam</a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice Blog post!</div>
-        </div>
-      </div>
+      <CommentDetail />
     </div>
   );
 };
