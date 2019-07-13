527a5fca25250a5baed65758775b5b6af6b9a5f7
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-13 14:21:42 +0530

    Implementing Approval Card

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 //. means it's present in the same folder
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam"/>
-      <CommentDetail author="Alex"/>
-      <CommentDetail author="Jane"/>
+    <ApprovalCard />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        content="Nice Post"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00PM"
+        content="What a post"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        content="Loved it!"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
